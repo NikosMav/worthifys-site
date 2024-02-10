a785509ae5b85750e277842cf287d4f808fab3f4
Anastasios084 <ta.saras@hotmail.com>
2024-02-11 00:08:41 +0200

    tinkering with the heatmap

--- my-react-app/src/components/charts/HeatMap.js
+++ my-react-app/src/components/charts/HeatMap.js
@@ -5,7 +5,10 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet.heat';
 import Papa from 'papaparse';
 import csvFile from './data/coordinates.csv'; // Adjust the path as necessary
-
+const bounds = [
+  [34.88, 19.37], // Southwestern corner of the bounding box
+  [41.75, 29.65]  // Northeastern corner of the bounding box
+];
 const Heatmap = () => {
   const [points, setPoints] = useState([]);
   const center = [37.983810, 23.727539]; // Adjust as needed
@@ -23,7 +26,7 @@ const Heatmap = () => {
   }, []);
 
   return (
-    <MapContainer center={center} zoom={6} className="leaflet-container">
+    <MapContainer center={center} zoom={6} minZoom={6} maxZoom={10} maxBounds={bounds} maxBoundsViscosity={1.0} className="leaflet-container">
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -35,19 +38,40 @@ const Heatmap = () => {
 
 const HeatLayer = ({ points }) => {
   const map = useMap();
-
+  let radiusOption = map.getZoom();
+  console.log(radiusOption)
 useEffect(() => {
+  radiusOption = map.getZoom();
+
   const heatLayer = L.heatLayer(points, {
-    radius: 20,
-    blur: 15,
+    radius: radiusOption*4,
+    blur: 10,
     maxZoom: 17,
-    gradient: {0.15: 'blue', 0.3: 'lime', 0.5: 'red'}
+    gradient: {0.18: 'blue', 0.22: 'lime', 0.3: 'red'}
   }).addTo(map);
 
-  // Adjust radius on zoom
+  // Adjust settings on zoom (zoom starts from 6 with current bounds)
   const onZoomEnd = () => {
-    const currentZoom = map.getZoom();
-    heatLayer.setOptions({ radius: currentZoom * 4 }); // Example scaling factor
+    // const currentZoom = map.getZoom();
+    radiusOption = map.getZoom();
+    let zoom = 17;
+    let blur = 10;
+    console.log(radiusOption)
+    radiusOption = radiusOption*4
+    
+    if (radiusOption > 7){
+      zoom = 16
+      // blur = 10
+      // radiusOption = radiusOption*6
+    }else{
+      radiusOption = radiusOption*4
+    }
+    heatLayer.setOptions({
+      radius: radiusOption,
+      blur: blur,
+      maxZoom: zoom,
+      gradient: {0.18: 'blue', 0.22: 'lime', 0.3: 'red'}
+    }); // Example scaling factor
   };
 
   map.on('zoomend', onZoomEnd);
